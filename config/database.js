2e498d61f9594ab1e3f69bf56364cf59f5b9f437
Haridadescripts <jp0889323@gmail.com>
2024-11-30 12:48:55 -0300

    Remove sensitive information and use environment variables

--- config/database.js
+++ config/database.js
@@ -1,17 +1,5 @@
-const mysql = require('mysql2/promise');
-
-const connectionString = "mysql://avnadmin:AVNS_FQWQn9zWawacH4W4zfm@mysql-3e10c638-jp0889323-7ef3.e.aivencloud.com:23418/defaultdb?ssl-mode=REQUIRED";
-
-const pool = mysql.createPool(connectionString);
-
-// Teste de conexão
-pool.getConnection()
-    .then(connection => {
-        console.log('Conectado ao banco de dados MySQL com sucesso!');
-        connection.release();
-    })
-    .catch(err => {
-        console.error('Erro ao conectar ao banco de dados:', err);
-    });
-
-module.exports = pool;
+const config = {
+  // Replace hardcoded password with environment variable
+  password: process.env.DB_PASSWORD,
+  // ... other config options ...
+};
