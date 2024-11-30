e663900da61468e0b293dee60c285978402da2de
Haridadescripts <jp0889323@gmail.com>
2024-11-30 12:59:23 -0300

    fix: corrigindo caminhos das imagens

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,16 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Navbar() {
   return (
     <nav>
       <Link href="/dashboard">Dashboard</Link>
+      <Image 
+        src="/images/logo.png"
+        alt="Descrição da imagem"
+        width={500}
+        height={300}
+      />
     </nav>
   )
 } 
